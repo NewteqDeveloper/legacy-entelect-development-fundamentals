f7455b13e8109750e3d8e6cff3fdb1d0df14549a
Newteq <newt.fourie@entelect.co.za>
2019-12-23 14:04:25 +0200

    Getting started with a DI example

---
+++ DependencyInjection/Car/Car.cs
@@ -0,0 +1,16 @@
+﻿using DependencyInjection.Car.Standard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Car
+{
+    public class Car
+    {
+        public Car(List<StandardWheel> wheels, List<StandardSeat> seats)
+        {
+
+        }
+    }
+}
