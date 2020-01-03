23c5b41a3a8068da30e06e88285870319339783f
Newteq <newt.fourie@entelect.co.za>
2020-01-03 12:31:32 +0200

    Some more examples

---
+++ Practical/MetaPrinciples/People/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaPrinciples.People
+{
+    public class Child : Person
+    {
+        public Mother mother { get; set; }
+        public Father father { get; set; }
+        public IList<Child> children { get; set; }
+    }
+}
---
+++ Practical/MetaPrinciples/People/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaPrinciples.People
+{
+    // least knowledge and some refactoring for YAGNI as mother, father and child all have the same thing as a person should have
+    public class Person
+    {
+        public string Location { get; set; }
+    }
+}
