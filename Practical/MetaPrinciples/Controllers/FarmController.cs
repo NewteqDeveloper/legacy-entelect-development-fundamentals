9e6b39da8249f901b124648b039eaf0b9c16f34d
Newteq <newt.fourie@entelect.co.za>
2020-01-03 12:54:43 +0200

    working on the api

--- Practical/MetaPrinciples/Controllers/FarmController.cs
+++ Practical/MetaPrinciples/Controllers/FarmController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MetaPrinciples.Animals;
+using MetaPrinciples.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,11 +13,13 @@ namespace MetaPrinciples.Controllers
     [Route("[controller]")]
     public class FarmController : ControllerBase
     {
-        private readonly ILogger<FarmController> _logger;
+        private readonly ILogger<FarmController> logger;
+        private readonly AnimalDb animalDb;
 
-        public FarmController(ILogger<FarmController> logger)
+        public FarmController(ILogger<FarmController> logger, AnimalDb animalDb)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.animalDb = animalDb;
 
             // this is the place for the animals to interact
             // maybe have list a static list of animals that can be created and they can interact with each other
@@ -26,6 +30,34 @@ namespace MetaPrinciples.Controllers
 
             // think about seperation of concerns and least knowledge too
             // least astonishment should be easy enough, with the open db connection in one method and the close in another
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public IList<Animal> GetAllAnimals()
+        {
+            return animalDb.Animals();
+        }
+
+        [HttpGet]
+        [Route("getAllCats")]
+        public List<Animal> GetAllCats()
+        {
+            return animalDb.Animals().Where(x => x.Type == AnimalType.Cat).ToList();
+        }
+
+        [HttpGet]
+        [Route("getDogs")]
+        public IEnumerable<Animal> GetAllDogs()
+        {
+            return animalDb.Animals().Where(x => x.Type == AnimalType.Dog);
+        }
+
+        [HttpGet]
+        [Route("getTheCowsOnTheFarm")]
+        public IActionResult GetAllCows()
+        {
+            return Ok(animalDb.Animals().Where(x => x.Type == AnimalType.Cow));
         }
     }
 }
