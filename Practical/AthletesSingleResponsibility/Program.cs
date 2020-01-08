246ba579ffa301eaab9ab37c4443c5ecd304d9e6
Newteq <newt.fourie@entelect.co.za>
2020-01-08 14:53:04 +0200

    remove comments and add console read

--- Practical/AthletesSingleResponsibility/Program.cs
+++ Practical/AthletesSingleResponsibility/Program.cs
@@ -8,19 +8,6 @@ namespace AtheletesSingleResponsibility
     {
         static void Main(string[] args)
         {
-            /* in here, the idea is to have an athelte that does EVERYTHING
-             * And then they have a compete method - which will do one of many of the items listed below
-             * This will be an abstract class - that contains all the methods!
-             * But the athelte has all the methods of sprints, long jump, high jump etc.
-             * 
-             * How to make it better?
-             * Then the Idea is to abstract these out to their own classes and create interface for ISprints, ILongJump, IHighJump
-             * and then in each of the different classes, they extend the interfaces and then in the compete method, it uses those methods from the interface
-             * 
-             */
-
-            Console.WriteLine("Hello World!");
-
             foreach(var athelete in Database.Atheletes)
             {
                 if(athelete.GetType() == typeof(HighJumper))
@@ -58,6 +45,8 @@ namespace AtheletesSingleResponsibility
                     athelete.Swim(SwimDistance.OneHundred);
                 }
             }
+
+            Console.ReadKey();
         }
     }
 }
