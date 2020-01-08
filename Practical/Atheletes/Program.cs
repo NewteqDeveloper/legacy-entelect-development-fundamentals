baaa2de67cb4eb621989017c34d16e1568ba4d5a
Newteq <newt.fourie@entelect.co.za>
2020-01-08 12:16:10 +0200

    Make the sample application runnable with some errors

--- Practical/Atheletes/Program.cs
+++ Practical/Atheletes/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AtheletesSingleResponsibility.Atheletes;
+using AtheletesSingleResponsibility.Data;
+using System;
 
 namespace AtheletesSingleResponsibility
 {
@@ -18,6 +20,44 @@ namespace AtheletesSingleResponsibility
              */
 
             Console.WriteLine("Hello World!");
+
+            foreach(var athelete in Database.Atheletes)
+            {
+                if(athelete.GetType() == typeof(HighJumper))
+                {
+                    athelete.HighJump();
+                }
+
+                if (athelete.GetType() == typeof(JavelinThrower))
+                {
+                    athelete.ThrowJavelin();
+                }
+
+                if (athelete.GetType() == typeof(LongJumper))
+                {
+                    athelete.LongJump();
+                    athelete.TripleJump();
+                }
+
+                if (athelete.GetType() == typeof(Runner))
+                {
+                    athelete.RunSprints(SprintDistance.OneHundred);
+                    athelete.RunRelay();
+                    athelete.RunHurdles();
+                    athelete.ThrowJavelin();
+                }
+
+                if (athelete.GetType() == typeof(ShotputThrower))
+                {
+                    athelete.ThrowShotput();
+                    athelete.ThrowJavelin();
+                }
+
+                if (athelete.GetType() == typeof(Swimmer))
+                {
+                    athelete.Swim(SwimDistance.OneHundred);
+                }
+            }
         }
     }
 }
