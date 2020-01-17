a1d79a3c92bd927c3fe76958f5d7fd03971163c3
Newteq <newt.fourie@entelect.co.za>
2020-01-17 14:43:47 +0200

    Trying to break liskov, but I can't do it actually

---
+++ Practical/SolidDojoLiskov/Printer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidDojoLiskov
+{
+    public class Printer
+    {
+        public void PrintShape(Shapes2.Shape shape)
+        {
+            var result = shape.Draw();
+            if (result.GetType() == typeof(string))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                throw new Exception("Invalid data sent to printer");
+            }
+        }
+    }
+}
--- Practical/SolidDojoLiskov/Program.cs
+++ Practical/SolidDojoLiskov/Program.cs
@@ -12,48 +12,63 @@ namespace SolidDojoLiskov
 
         static void Main(string[] args)
         {
-            var runOption = RunOptions.ShapesNewOverrideInt;
+            //var runOption = RunOptions.ShapesNewOverrideInt;
 
-            if (runOption == RunOptions.ShapesNewOverride)
-            {
-                #region ShapesNewOverride
-                List<Shapes1.Shape> shapes = new List<Shapes1.Shape>
-                {
-                    new Shapes1.Circle(),
-                    new Shapes1.Square(),
-                };
+            //if (runOption == RunOptions.ShapesNewOverride)
+            //{
+            //    #region ShapesNewOverride
+            //    List<Shapes1.Shape> shapes = new List<Shapes1.Shape>
+            //    {
+            //        new Shapes1.Circle(),
+            //        new Shapes1.Square(),
+            //    };
 
-                Console.WriteLine("Polymorphic Draw:");
-                foreach (var shape in shapes)
-                {
-                    Console.WriteLine($"Shape: {shape.GetType().Name} -- {shape.Draw()}");
-                }
-                Console.WriteLine("==============================");
-                Console.WriteLine("new Square Draw:");
-                Console.WriteLine(new Shapes1.Square().Draw());
+            //    Console.WriteLine("Polymorphic Draw:");
+            //    foreach (var shape in shapes)
+            //    {
+            //        Console.WriteLine($"Shape: {shape.GetType().Name} -- {shape.Draw()}");
+            //    }
+            //    Console.WriteLine("==============================");
+            //    Console.WriteLine("new Square Draw:");
+            //    Console.WriteLine(new Shapes1.Square().Draw());
 
-                #endregion ShapesNewOverride
-            }
+            //    #endregion ShapesNewOverride
+            //}
 
-            // MOST EXTREME CASE - because draw does not do at all what the base class does
-            if (runOption == RunOptions.ShapesNewOverrideInt)
-            {
-                #region ShapesNewOverrideInt
-                List<Shapes2.Shape> shapes2 = new List<Shapes2.Shape>
+            //// MOST EXTREME CASE - because draw does not do at all what the base class does
+            //if (runOption == RunOptions.ShapesNewOverrideInt)
+            //{
+            //    #region ShapesNewOverrideInt
+            //    List<Shapes2.Shape> shapes2 = new List<Shapes2.Shape>
+            //    {
+            //        new Shapes2.Circle(),
+            //        new Shapes2.Square(),
+            //    };
+
+            //    Console.WriteLine("Polymorphic Draw:");
+            //    foreach (var shape in shapes2)
+            //    {
+            //        Console.WriteLine($"Draw shape: {shape.GetType().Name} -- {shape.Draw()}");
+            //    }
+            //    Console.WriteLine("==============================");
+            //    Console.WriteLine("new Square Draw:");
+            //    Console.WriteLine(new Shapes2.Square().Draw());
+            //    #endregion ShapesNewOverrideInt
+            //}
+
+            var printer = new Printer();
+            printer.PrintShape(new Shapes2.Square());
+
+            List<Shapes2.Shape> shapes2 = new List<Shapes2.Shape>
                 {
                     new Shapes2.Circle(),
                     new Shapes2.Square(),
                 };
 
-                Console.WriteLine("Polymorphic Draw:");
-                foreach (var shape in shapes2)
-                {
-                    Console.WriteLine($"Draw shape: {shape.GetType().Name} -- {shape.Draw()}");
-                }
-                Console.WriteLine("==============================");
-                Console.WriteLine("new Square Draw:");
-                Console.WriteLine(new Shapes2.Square().Draw());
-                #endregion ShapesNewOverrideInt
+            Console.WriteLine("Polymorphic Draw:");
+            foreach (var shape in shapes2)
+            {
+                printer.PrintShape(shape);
             }
 
             Console.ReadKey();
